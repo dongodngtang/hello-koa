4a36ed3a3be54a8b2f53980f19283048a71de4a9
lorne <2270333671@qq.com>
2017-10-27 15:54:46 +0800

    init node.js

---
+++ controller.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+
+function addMapping(router, mapping) {
+    for (var url in mapping) {
+        if (url.startsWith('GET ')) {
+            //如果url类似“GET xxx"
+            var path = url.substring(4);
+            router.get(path, mapping[url]);
+            console.log(`reqister URL mapping:GET ${path}`);
+        } else if (url.startsWith('POST ')) {
+            var path = url.substring(5);
+            router.post(path, mapping[url]);
+            console.log(`register URL mapping:POST ${path}`)
+
+        } else {
+            console.log(`invalid URL: ${url}`)
+        }
+    }
+}
+
+function addControllers(router) {
+    //这里可以用sync是因为启动时只运行一次，不存在性能问题
+    var files = fs.readdirSync(__dirname + '/controllers');
+
+    // 过滤出.js 文件
+    var js_files = files.filter(f => {
+        return f.endsWith('.js')
+    })
+    // 处理每个js文件
+    for (var f of js_files) {
+        console.log(`process controller:${f}...`)
+        //导入js文件
+        let mapping = require(__dirname + '/controllers/' + f);
+        addMapping(router, mapping);
+
+    }
+}
+
+module.exports = function(dir){
+    let controllers_dir = dir || 'controllers',
+    router = require('koa-router')();
+    addControllers(router,controllers_dir);
+    return router.routes();
+}
